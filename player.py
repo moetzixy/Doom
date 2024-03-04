522c5088b46c39efcd082888c2f194de96fe738a
Heinzle Moritz <moritz.heinzle@bulme.at>
2024-03-04 17:22:20 +0100

    Movement funktion in player.py

---
+++ player.py
@@ -0,0 +1,53 @@
+from settings import *
+import pygame as pg
+import math
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        keys = pg.key.get_pressed()
+        num_key_pressed = -1
+        if keys[pg.K_w]:
+            num_key_pressed += 1
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pg.K_s]:
+            num_key_pressed += 1
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pg.K_a]:
+            num_key_pressed += 1
+            dx += speed_sin
+            dy += -speed_cos
+        if keys[pg.K_d]:
+            num_key_pressed += 1
+            dx += -speed_sin
+            dy += speed_cos
+
+        self.x += dx
+        self.y += dy
+
+        if keys[pg.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pg.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+    def update(self,):
+        self.movement()
+
+    @property
+    def pos(self):
+        return self.x, self.y
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
